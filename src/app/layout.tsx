266964881005adc16c451f6fd4ba83143aa0d3ec
Deepak <206972401+thedeepak12@users.noreply.github.com>
2026-01-01 20:12:56 +0530

    Initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Where's Waldo",
+  description: "Where's Waldo is an interactive puzzle game where players search for a character, Waldo, hidden within complex, cluttered scenes filled with distractions.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
