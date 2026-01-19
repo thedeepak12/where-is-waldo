b261fb9b11f912be861f375163ecce70ae0a9920
Deepak <206972401+thedeepak12@users.noreply.github.com>
2026-01-20 00:55:25 +0530

    Fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import GameContainer from '../components/game/GameContainer';
 import { db } from '../db';
 import { characters } from '../db/schema';
 
+export const dynamic = 'force-dynamic';
+export const runtime = 'nodejs';
+
 export default async function Home() {
   const allCharacters = await db.select().from(characters);
 
